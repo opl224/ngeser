e0fa6d7203a4ecdab40ca1f8ccc29297b0cf722f
Opal Aja <opank2441@gmail.com>
2025-06-01 13:25:42 +0000

    Error: A param property was accessed directly with `params.userId`. `params` is now a Promise and should be unwrapped with `React.use()` before accessing properties of the underlying params object. In this version of Next.js direct access to param properties is still supported to facilitate migration but in a future version you will be required to unwrap `params` with `React.use()`.     at createUnhandledError (https://6000-firebase-studio-1748783613696.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:879:71)     at handleClientError (https://6000-firebase-studio-1748783613696.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:1052:56)     at console.error (https://6000-firebase-studio-1748783613696.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_client_43e3ffb8._.js:1191:56)     at warnForSyncAccess (https://6000-firebase-studio-1748783613696.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_1a6ee436._.js:854:13)     at Object.get (https://6000-firebase-studio-1748783613696.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_1a6ee436._.js:834:21)     at ProfilePage (https://6000-firebase-studio-1748783613696.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev/_next/static/chunks/src_7fab0013._.js:1667:28)     at ClientPageRoot (https://6000-firebase-studio-1748783613696.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_1a6ee436._.js:2053:50)

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import { UserProfileDisplay } from '@/components/UserProfileDisplay';
+import { useParams } from 'next/navigation';
+import React from 'react'; // Import React
 
 interface ProfilePageProps {
-  params: {
-    userId: string;
-  };
+  params: Promise<{ userId: string }>; // params is a Promise
 }
 
 export default function ProfilePage({ params }: ProfilePageProps) {
+  // Unwrap the params Promise using React.use()
+  const actualParams = React.use(params);
+
   return (
     <div className="container mx-auto py-8">
-      <UserProfileDisplay userId={params.userId} />
+      <UserProfileDisplay userId={actualParams.userId} />
     </div>
   );
 }
